88026fc91b4753c7bdf58f7ad56b4490824ffe18
Ratul Mukherjee <ratul.lamborghini@gmail.com>
2018-03-21 17:58:23 +0530

    Added Popup for Role Entry

--- public/chat.js
+++ public/chat.js
@@ -1,21 +1,36 @@
 // Make connection
 var socket = io.connect('http://localhost:4000');
     $(document).ready(function () {
-        $(".btn").click(function (e) { 
+        if(localStorage.getItem('Handle')==undefined){
+            $("#signup").modal('show');
+        }
+       
+        //var roll_count=""
+       $("#savedetails").click(function (e) { 
+           e.preventDefault();
+           localStorage.setItem('token',$('input[name=Radio]:checked').val());
+           localStorage.setItem('Handle',$("#user").val())
+
+           $("#signup").modal('hide');
+       
+           
+       });
+
+        $(".btn-sm").click(function (e) { 
             e.preventDefault();
-            var roll_count =$(this).attr('id').substring(1);
+              var roll_count =$(this).attr('id').substring(1);
            
             socket.emit('roll', {
-                dice: roll_count, 
+                dice: roll_count,
                 roll: Math.floor((Math.random() * parseInt(roll_count)) + 1),
-                handle: $("#handle").val()
+                handle: localStorage.getItem('Handle')
             });
 
 
         });
 
             socket.on('roll', function(data){
-                var str= '<p><strong>' + data.handle + ' has just rolled a D'+roll_count+'</strong>' + data.roll + '</p>';
+                var str= '<p><strong>' + data.handle + ' has just rolled a D'+data.dice +'</strong> ' + data.roll + '</p>';
                 $("#output").append(str);
             });
 
